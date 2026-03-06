90ccb70fb417f0bc9c07402865a4597d9a8d7e87
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2026-03-06 12:17:51 +0000

    add extension link in footer

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { COLORS } from '../../constants/theme';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
@@ -7,11 +8,11 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
                 <div className="flex items-center justify-between">
                     <p className="text-sm text-gray-500">
-                        © {new Date().getFullYear()} Hi-M. Solutek. All rights reserved.
+                        © {new Date().getFullYear()} Hi-M.Solutek. All rights reserved.
                     </p>
-                    <p className="text-sm text-gray-500">
-                        Powered by Frappe CRM
-                    </p>
+                    <Link to="https://extensionerp.com" target='_blank' className="text-sm text-gray-500">
+                        Powered by Extension ERP
+                    </Link>
                 </div>
             </div>
         </footer>
